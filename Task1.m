8dd3e41884fb2607becb941af9a50a5d0a2bf833
don <misurakalu@gmail.com>
2021-08-16 14:42:13 +1000

    Assessment update

---
+++ Task1.m
@@ -0,0 +1,11 @@
+%% Clear script %%
+clear ;clc ;close all ;
+  
+%% Task 1 
+
+% Import the dataset
+data = readtable('n10496262.csv');
+
+% Perform preliminary checks for data consistency and completeness
+
+% 
